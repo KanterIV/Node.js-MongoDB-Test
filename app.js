2b13c764fd289b1f70d136fd5dfc8874f1f1743f
KanterIV <126972573+KanterIV@users.noreply.github.com>
2023-12-14 09:53:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-// const contactsRouter = require("./routes/api/contacts");
+const contactsRouter = require("./routes/api/contacts");
 
 dotenv.config();
 
@@ -15,7 +15,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-// app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
